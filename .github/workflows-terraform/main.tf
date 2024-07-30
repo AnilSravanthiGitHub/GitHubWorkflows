a7038ae76a7a7bc0656c5f8f96c9d677d0d9f581
AnilSravanthi <balaih3pothula@gmail.com>
2024-07-30 20:25:14 +0200

    terraform

---
+++ .github/workflows-terraform/main.tf
@@ -0,0 +1,49 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.7.0"
+    }
+  }
+
+backend "azurerm" {
+    key = "terraform.tfstate"
+  }
+  
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "resgrp" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_app_service_plan" "asp" {
+  name                = var.app_service_plan_name
+  location            = azurerm_resource_group.resgrp.location
+  resource_group_name = azurerm_resource_group.resgrp.name
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+}
+
+resource "azurerm_app_service" "appsvc" {
+  name                = var.app_service_name
+  location            = azurerm_resource_group.resgrp.location
+  resource_group_name = azurerm_resource_group.resgrp.name
+  app_service_plan_id = azurerm_app_service_plan.asp.id
+
+  site_config {
+    dotnet_framework_version = "v8.0"
+  }
+
+  app_settings = {
+    "WEBSITE_RUN_FROM_PACKAGE" = "1"
+  }
+}
+
+output "app_service_default_hostname" {
+  value = azurerm_app_service.appsvc.default_site_hostname
+}
