650e86710619aff87021c921e4145b1ab30be888
AnilSravanthi <balaih3pothula@gmail.com>
2024-07-31 08:16:31 +0200

    changes in variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,23 +1,23 @@
 variable "resource_group_name" {
   description = "Name of the resource group"
   type        = string
-  default     = var.resource_group_name
+  
 }
 
 variable "location" {
   description = "Azure location"
   type        = string
-  default     =  var.location
+  
 }
 
 variable "app_service_plan_name" {
   description = "Name of the App Service plan"
   type        = string
-  default     = var.app_service_plan_name
+  
 }
 
 variable "app_service_name" {
   description = "Name of the App Service"
   type        = string
-  default     = var.app_service_name
+  
 }
