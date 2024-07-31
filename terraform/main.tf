639b8e54678ba09c8fa0e484763373fd51aac19c
AnilSravanthi <balaih3pothula@gmail.com>
2024-07-31 17:52:02 +0200

    storage account

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,18 +19,39 @@ provider "azurerm" {
   skip_provider_registration = "true"
 }
 
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# Storage Account
+resource "azurerm_storage_account" "storage" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+# Storage Container
+resource "azurerm_storage_container" "container" {
+  name                  = "terraform-state"
+  storage_account_name  = azurerm_storage_account.storage.name
+  container_access_type = "private"
+}
+
 resource "azurerm_service_plan" "asp" {
   name                = var.app_service_plan_name
-  location            = var.location
-  resource_group_name = var.resource_group_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Windows"
   sku_name            = "P1v2"
 }
 
 resource "azurerm_app_service" "appsvc" {
   name                = var.app_service_name
-  location            = var.location
-  resource_group_name = var.resource_group_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_service_plan.asp.id
 
   site_config {
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,21 +13,7 @@ variable "location" {
 variable "storage_account_name" {
     type    = string
 }
-
-variable "storage_account_tier" {
-    type    = string
-}
-
-variable "storage_account_replication_type" {
-    type    = string
-}
-
-
 variable "storage_container_name" {
-    type    = string
-}
-
-variable "container_access_type" {
     type    = string
 }
 
