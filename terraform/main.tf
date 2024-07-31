76d55def8054f631e0f7003aceaf152ce1bc4530
AnilSravanthi <balaih3pothula@gmail.com>
2024-07-31 20:00:37 +0200

    roll

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,8 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 3.7.0"
     }
-  }  
+  }
+  
 }
  
 provider "azurerm" {
@@ -13,6 +14,26 @@ provider "azurerm" {
   skip_provider_registration = "true"
 }
 
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# Storage Account
+resource "azurerm_storage_account" "storage" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+# Storage Container
+resource "azurerm_storage_container" "container" {
+  name                  = "terraform-state"
+  storage_account_name  = azurerm_storage_account.storage.name
+  container_access_type = "private"
+}
 
 terraform{
 backend "azurerm" {
