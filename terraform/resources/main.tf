e99ba8d678a42b0e30cae8582bf82b442681e857
AnilSravanthi <balaih3pothula@gmail.com>
2024-08-02 00:01:12 +0200

    backend "azurerm"

--- terraform/resources/main.tf
+++ terraform/resources/main.tf
@@ -13,6 +13,15 @@ provider "azurerm" {
   skip_provider_registration = "true"
 }
 
+terraform {
+  backend "azurerm" {
+    resource_group_name  = var.resource_group_name
+    storage_account_name = var.storage_account_name
+    container_name       = var.container_name
+    key                  = var.key
+  }
+}
+
 data "azurerm_resource_group" "rg" {
   name                              = var.resource_group_name
 }
