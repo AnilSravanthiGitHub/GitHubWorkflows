0c72889be3503c44f5f7d3630808ac4f31ce545e
AnilSravanthi <balaih3pothula@gmail.com>
2024-08-01 14:56:21 +0200

    rolling build with tf changes.

--- terraform/storage/main.tf
+++ terraform/storage/main.tf
@@ -14,28 +14,13 @@ provider "azurerm" {
   skip_provider_registration = "true"
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
-}
-
-# Storage Account
-resource "azurerm_storage_account" "storage" {
-  name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  tags = {
-    environment = "Terraform"
+terraform {
+  backend "azurerm" {
+    resource_group_name  = var.backend_resource_group_name
+    storage_account_name = var.backend_storage_account_name
+    container_name       = var.backend_container_name
+    key                  = "terraform.tfstate"
   }
-}
-
-# Storage Container
-resource "azurerm_storage_container" "container" {
-  name                  = "terraform-state"
-  storage_account_name  = azurerm_storage_account.storage.name
-  container_access_type = "private"
 }
 
 resource "azurerm_service_plan" "asp" {
