b1e77af2a57f9b22c9097910cc0d59146e048fa6
AnilSravanthi <balaih3pothula@gmail.com>
2024-08-01 17:32:03 +0200

    rolling build with rg name in backend rm

--- terraform/resources/main.tf
+++ terraform/resources/main.tf
@@ -15,7 +15,7 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-    resource_group_name  = ${var.resource_group_name}
+    resource_group_name  = "rg-se-terrraform-dev"
     storage_account_name = ${var.storage_account_name}
     container_name       = ${var.container_name}
     key                  = "terraform.tfstate"
