02624f9cad686b0e69c2a9b350cc81236f557c2a
AnilSravanthi <balaih3pothula@gmail.com>
2024-08-03 02:45:54 +0200

    sensitive

--- terraform/resources_aks/outputs.tf
+++ terraform/resources_aks/outputs.tf
@@ -8,35 +8,35 @@ output "kubernetes_cluster_name" {
 
 output "client_certificate" {
    value	= azurerm_kubernetes_cluster.k8s.kube_config[0].client_certificate
-   senstive = true
+   sensitive = true
 }
 
 output "client_key" {
    value    = azurerm_kubernetes_cluster.k8s.kube_config[0].client_key
-   senstive = true
+   sensitive = true
 }
 
 output "cluster_ca_certificate" {
    value    = azurerm_kubernetes_cluster.k8s.kube_config[0].cluster_ca_certificate
-   senstive = true
+   sensitive = true
 }
 
 output "cluster_username" {
    value    = azurerm_kubernetes_cluster.k8s.kube_config[0].username
-   senstive = true
+   sensitive = true
 }
 
 output "cluster_password" {
    value    = azurerm_kubernetes_cluster.k8s.kube_config[0].password
-   senstive = true
+   sensitive = true
 }
 
 output "host" {
    value    = azurerm_kubernetes_cluster.k8s.kube_config[0].host
-   senstive = true
+   sensitive = true
 }
 
 output "kube_config" {
   value    = azurerm_kubernetes_cluster.k8s.kube_config_raw
-  senstive = true
+  sensitive = true
 }
