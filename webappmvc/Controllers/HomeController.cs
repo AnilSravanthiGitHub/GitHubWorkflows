652ad1bfe5626b61782b4b9628f9f0fcda28c611
AnilSravanthi <balaih3pothula@gmail.com>
2025-03-02 18:51:15 +0530

    Update HomeController.cs
    add coment

--- webappmvc/Controllers/HomeController.cs
+++ webappmvc/Controllers/HomeController.cs
@@ -26,6 +26,7 @@ namespace webappmvc.Controllers
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
 		{
+  			// This is a comment
 			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
 		}
 	}
